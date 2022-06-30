3297666c00bbe1418b0088e95cdbf78248868661
omidkarami77 <omidtkd77@gmail.com>
2022-06-30 21:39:13 +0330

    design HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(color: Colors.red),
+      body: Container(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50),
+                  child: Text(
+                    "Mountain View",
+                    style: TextStyle(color: Colors.white, fontSize: 35),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    "Clear Sky",
+                    style: TextStyle(color: Colors.grey, fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Icon(
+                    Icons.wb_sunny_outlined,
+                    color: Colors.white,
+                    size: 80,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Text(
+                    "14" + "\u00b0",
+                    style: TextStyle(color: Colors.white, fontSize: 60),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "max",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Text(
+                            "14" + "\u00b0",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Container(
+                        width: 1,
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "min",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Text(
+                            "14" + "\u00b0",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          color: Colors.black),
     );
   }
 }
