320c737e8926a7d73f3d57afe3c0eee420a0ded4
omidkarami77 <omidtkd77@gmail.com>
2022-07-02 16:54:30 +0330

    add list and background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,6 +25,13 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("images/pic_bg.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
           child: Center(
             child: Column(
               children: [
@@ -70,7 +79,7 @@ class MyApp extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.only(top: 5.0),
                           child: Text(
-                            "14" + "\u00b0",
+                            "16" + "\u00b0",
                             style: TextStyle(color: Colors.white, fontSize: 20),
                           ),
                         ),
@@ -103,11 +112,171 @@ class MyApp extends StatelessWidget {
                       ],
                     ),
                   ],
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1,
+                    width: double.infinity,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Center(
+                      child: ListView.builder(
+                        itemCount: 6,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int position) {
+                          return Container(
+                            height: 50,
+                            width: 70,
+                            child: Card(
+                              color: Colors.transparent,
+                              child: Column(children: [
+                                Text(
+                                  "fri ,8pm",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 15),
+                                ),
+                                Icon(
+                                  Icons.cloud,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  "14",
+                                  style: TextStyle(
+                                      color: Colors.grey, fontSize: 20),
+                                ),
+                              ]),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1,
+                    width: double.infinity,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "wind speed",
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "4.73 m/s",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 10, top: 10, right: 10),
+                      child: Container(
+                        color: Colors.white,
+                        height: 40,
+                        width: 1,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "sunrise",
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "6:19 PM",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 10, top: 10, right: 10),
+                      child: Container(
+                        color: Colors.white,
+                        height: 40,
+                        width: 1,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "sunset",
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "9:3 AM",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 10, top: 10, right: 10),
+                      child: Container(
+                        color: Colors.white,
+                        height: 40,
+                        width: 1,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "humidity",
+                            style: TextStyle(color: Colors.grey, fontSize: 15),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Text(
+                            "72%",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
-          color: Colors.black),
+        ),
+      ),
     );
   }
 }
